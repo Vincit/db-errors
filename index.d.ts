6f4dd0f609502e670fba1a5217089295b975e5c1
Petr Hanak <jsem@petr-hanak.cz>
2019-01-11 21:56:24 +0100

    Fix client type

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,24 @@
 declare namespace DbTypes {
+    type ClientType = 'mysql' | 'sqlite' | 'postgres';
 
     interface DbErrorOpts {
         nativeError: {
             message: string
         }
+        client: ClientType
     }
 
     class DBError extends Error {
         name: string;
         nativeError: Error;
-        client: any;
+        client: ClientType;
 
-        constructor(args?: DbErrorOpts);
+        constructor(args?: DbErrorOpts)
     }
 
     class CheckViolationError extends DBError {
-        table: string
-        constraint: string
+        table: string;
+        constraint: string;
     }
 
     class ConstraintViolationError extends DBError {}
@@ -24,19 +26,19 @@ declare namespace DbTypes {
     class DataError extends DBError {}
 
     class ForeignKeyViolationError extends ConstraintViolationError {
-        table: string
-        constraint: string
+        table: string;
+        constraint: string;
     }
 
     class NotNullViolationError extends ConstraintViolationError {
-        table: string
-        constraint: string
+        table: string;
+        constraint: string;
     }
 
     class UniqueViolationError extends ConstraintViolationError {
-        table: string
-        columns: string[]
-        constraint: string
+        table: string;
+        columns: string[];
+        constraint: string;
     }
 
     function wrapError(err: Error): DBError

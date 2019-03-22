05a64604863578b0c0de39f6d6c41185598960ac
Petr Hanák <jsem@petr-hanak.cz>
2019-03-22 11:26:26 +0100

    Indent with 2 spaces

--- index.d.ts
+++ index.d.ts
@@ -1,47 +1,47 @@
 declare namespace DbTypes {
 
-    class DBError extends Error {
-        name: string;
-        nativeError: Error;
-    }
+  class DBError extends Error {
+    name: string;
+    nativeError: Error;
+  }
 
-    class CheckViolationError extends DBError {
-        table: string;
-        constraint: string;
-    }
+  class CheckViolationError extends DBError {
+    table: string;
+    constraint: string;
+  }
 
-    class ConstraintViolationError extends DBError {}
+  class ConstraintViolationError extends DBError {}
 
-    class DataError extends DBError {}
+  class DataError extends DBError {}
 
-    class ForeignKeyViolationError extends ConstraintViolationError {
-        table: string;
-        constraint: string;
-    }
+  class ForeignKeyViolationError extends ConstraintViolationError {
+    table: string;
+    constraint: string;
+  }
 
-    class NotNullViolationError extends ConstraintViolationError {
-        table: string;
-        column: string;
-    }
+  class NotNullViolationError extends ConstraintViolationError {
+    table: string;
+    column: string;
+  }
 
-    class UniqueViolationError extends ConstraintViolationError {
-        table: string;
-        columns: string[];
-        constraint: string;
-    }
+  class UniqueViolationError extends ConstraintViolationError {
+    table: string;
+    columns: string[];
+    constraint: string;
+  }
 
-    function wrapError(err: Error): DBError
+  function wrapError(err: Error): DBError
 
-    export {
-        wrapError,
+  export {
+    wrapError,
 
-        CheckViolationError,
-        ConstraintViolationError,
-        DataError,
-        ForeignKeyViolationError,
-        NotNullViolationError,
-        UniqueViolationError,
-    }
+    CheckViolationError,
+    ConstraintViolationError,
+    DataError,
+    ForeignKeyViolationError,
+    NotNullViolationError,
+    UniqueViolationError,
+  }
 }
 
 export = DbTypes
